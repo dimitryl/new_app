339caade07520658ba3448f35f0a704a77eb71a1
Dimitry Lukyanov <dimitry.lukyanov@healthagen.com>
2013-09-06 12:35:05 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewApp::Application.config.secret_token = '81f57bdfc28161ed23342fd663a13c2fe8585b7243cca6e28925f0ddf550e997ffb37406f162d1097f44e7b0bdb724a3e87effaac126c949e0593cabb56dabda'
